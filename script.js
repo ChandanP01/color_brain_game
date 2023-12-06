1f54d878f94aa7c0227f3277634c186234eb42b5
Chandan Kumar Parida <120441215+ChandanP01@users.noreply.github.com>
2023-12-06 18:08:17 +0530

    Update script.js to add play sounds for wrong choice and right choice

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let color;
 let word;
 let true_count = 0;
 let false_count = 0;
+let right_choice = new Audio("right.unknown");
+let wrong_choice = new Audio("wrong.unknown");
 
 function Timer() {
     const timeInterval = setInterval(() => {
@@ -16,7 +18,7 @@ function Timer() {
             clearInterval(timeInterval);
             document.querySelector('#tail').style.display = "none";
             document.querySelector('#result').textContent = "Refresh The Page For Play Again";
-            document.querySelector('#middle').innerHTML = `<p>Good = ${true_count}</p> <p>Bad = ${false_count}</p> <p>Score = ${true_count-false_count}</p>`
+            document.querySelector('#middle').innerHTML = `<p>Good = ${true_count}</p> <p>Bad = ${false_count}</p> <p>Score = ${true_count - false_count}</p>`
             document.querySelector('#middle').style.color = "#fff";
         }
     }, 1000);
@@ -34,13 +36,15 @@ function checkResult() {
     document.querySelector('#tail').addEventListener('click', (e) => {
         const clicked_color = e.target.textContent;
         if (clicked_color === color) {
+            right_choice.play();
             true_count++;
             generateWord();
         } else {
+            wrong_choice.play();
             false_count++;
             generateWord();
         }
-    })
+    });
 }
 
 Timer();
